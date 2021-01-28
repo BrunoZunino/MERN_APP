15c8d11d67c1dc4823d206c6aee0c3c3986564c9
BrunoZunino <bzuninofontela@gmail.com>
2021-01-27 22:16:45 -0300

    day 2

--- react-frontend-01-starting-setup/src/user/pages/User.js
+++ react-frontend-01-starting-setup/src/user/pages/User.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-    return <h2>Users Works </h2>
+
+    const USERS = [{
+        id: 'u1',
+        name: 'Bruno',
+        image: 'https://www.pexels.com/photo/ocean-waves-under-white-sky-4737039/',
+        places: 3
+    },];
+
+    return <UsersList items = {
+        USERS
+    }
+    />
 };
 
 export default Users;

b3be46cdf0d5e21a790fe1326f9b3371e60cbbe0
BrunoZunino <bzuninofontela@gmail.com>
2021-02-02 22:52:09 -0300

    day 5

--- react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
+++ react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList'
 
@@ -15,11 +16,28 @@ const DUMMY_PLACES = [
       lng: 2342423423
     },
     creator: 'u1'
+  },
+
+  {
+    id: 'p2',
+    title: 'Empire States',
+    description: 'NEW YORK GREATEST BUILDING',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
+    address: 'NEW YORK',
+    location: {
+      lat: 2354234,
+      lng: 2342423423
+    },
+    creator: 'u2'
   }
 ]
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES}/>
+  const userId = useParams().userId;
+
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
+  return <PlaceList items={loadedPlaces}/>
 }
 
 export default UserPlaces;

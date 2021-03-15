899bf532f2e580bb9efc003891915d1ad6c303dd
BrunoZunino <bruno.zunino@anima.edu.uy>
2021-03-15 10:46:19 -0300

    Add files via upload

--- NewPlace.js
+++ NewPlace.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import Input from '../../shared/components/FormElements/Input'
-import './NewPlaces.css'
+import './NewPlace.css'
 
 const NewPlace = () => {
     return <form className='place-form'>
-        <Input element='input' type='text' label='Title'/>
+        <Input element='input' type='text' label='Title' validators={[]} errorText='Please enter a valid title'/>
     </form>
 };
 
